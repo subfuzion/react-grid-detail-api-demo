05331078f055a8871fde540a750f14f08c53ffd8
Cormac Pujals <cmpujals@gmail.com>
2023-02-16 12:43:13 -0800

    adds detailPage img max height and width

--- client/src/components/ProductDetailPage/ProductDetail.jsx
+++ client/src/components/ProductDetailPage/ProductDetail.jsx
@@ -7,7 +7,7 @@ export default function ProductDetail({products}) {
   return (
     <>
       <h1>{product.name}</h1>
-      <img src={product.picture} alt=""/>
+      <img src={product.picture} alt="" style={{maxHeight: "40vh", maxWidth: "40vw"}}/>
       <p>Description: {product.description}</p>
       <Link to="/"><button>All Products</button></Link>
     </>
